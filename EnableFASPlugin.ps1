4a748214656b7f75f30c39161362563adda9a807
Landon Fraley <landon@jenndon.org>
2016-11-08 11:24:45 -0500

    Better comments/console output

--- EnableFASPlugin.ps1
+++ EnableFASPlugin.ps1
@@ -6,7 +6,7 @@ Set-ExecutionPolicy Bypass -Scope process
 Write-Host "Importing StoreFront Modules"
 Get-Module "Citrix.StoreFront.*" -ListAvailable | Import-Module
 
-Write-Host "Updating plugin for $StoreVirtualPath"
+Write-Host "Updating StoreFront Auth plugin for $StoreVirtualPath"
 $store = Get-STFStoreService -VirtualPath $StoreVirtualPath
 $auth = Get-STFAuthenticationService -StoreService $store
 Set-STFClaimsFactoryNames -AuthenticationService $auth -ClaimsFactoryName "FASClaimsFactory"
