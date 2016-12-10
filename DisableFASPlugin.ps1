3ba560965e3e13c735a152c50386d762a529730b
Landon Fraley <landon@jenndon.org>
2016-12-10 01:13:29 -0500

    Update DisableFASPlugin.ps1

--- DisableFASPlugin.ps1
+++ DisableFASPlugin.ps1
@@ -1,5 +1,11 @@
+param (
+    [Parameter(Mandatory=$true)][string]$StoreVirtualPath
+)
+
+Set-ExecutionPolicy Bypass -Scope process
+Write-Host "Importing StoreFront Modules"
 Get-Module "Citrix.StoreFront.*" -ListAvailable | Import-Module
-$StoreVirtualPath = "/Citrix/Store"
+
 $store = Get-STFStoreService -VirtualPath $StoreVirtualPath
 $auth = Get-STFAuthenticationService -StoreService $store
 Set-STFClaimsFactoryNames -AuthenticationService $auth -ClaimsFactoryName "standardClaimsFactory"
