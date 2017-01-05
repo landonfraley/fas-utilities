4f44a3001c3c352616d7ea74b9313154bf0e780f
Landon Fraley <landon@jenndon.org>
2017-01-05 14:10:29 -0500

    Added comments/help documentation

--- EnableFASPlugin.ps1
+++ EnableFASPlugin.ps1
@@ -1,3 +1,20 @@
+<#
+.SYNOPSIS
+
+Configures the StoreFront store, specified by the StoreVirtualPath, with the FAS LogonDataProvider and CredentialPlugin.
+
+.DESCRIPTION
+
+Based on commands from Citrix Documentation, http://docs.citrix.com/en-us/xenapp-and-xendesktop/7-12/secure/federated-authentication-service.html, the script will update the LogonDataProvider and CredentialPlugin for the StoreFront store associated with the IIS Virtual Path (StoreVirtualPath) passed to the script.
+
+.PARAMETER StoreVirtualPath
+
+The IIS Virtual Path for the StoreFront store being updated, includes the leading '/'.
+
+.EXAMPLE
+EnableFASPlugin.ps1 "/Citrix/Store"
+
+#>
 param (
     [Parameter(Mandatory=$true)][string]$StoreVirtualPath
 )
@@ -6,7 +23,7 @@ Set-ExecutionPolicy Bypass -Scope process
 Write-Host "Importing StoreFront Modules"
 Get-Module "Citrix.StoreFront.*" -ListAvailable | Import-Module
 
-Write-Host "Updating StoreFront Auth plugin for $StoreVirtualPath"
+Write-Host "Updating LogonDataProvider/CredentialPlugin for $StoreVirtualPath"
 $store = Get-STFStoreService -VirtualPath $StoreVirtualPath
 $auth = Get-STFAuthenticationService -StoreService $store
 Set-STFClaimsFactoryNames -AuthenticationService $auth -ClaimsFactoryName "FASClaimsFactory"
